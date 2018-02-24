9045c53688502c3bce20633ee541999d68b9e045
Trym Skaar <trym.skaar@gmail.com>
2018-02-24 04:06:07 +0100

    Prepare for publishing with compat build

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
+FROM trym/watchexec-alpine as watchexec
 FROM node:9.6.1-alpine
+
+COPY --from=watchexec /bin/watchexec /bin
+RUN apk -U add bash git
 
 # Include node_modules/.bin in PATH for not having to prefix commands
 ENV PATH=$PATH:/ddbes/node_modules/.bin
--- test/configs/aws.js
+++ test/configs/aws.js
@@ -1,11 +1,11 @@
-import ddbes from '../../lib'
-import AWS from 'aws-sdk'
+const config = require('../../lib/config')
+const AWS = require('aws-sdk')
 
 AWS.config.update({
   region: process.env.AWS_DEFAULT_REGION || 'eu-west-1',
 })
 
-Object.assign(ddbes.config, {
+Object.assign(config, {
   tableName: process.env.TABLE_NAME || 'ddbes-tests',
   snapshots: true,
   snapshotsBucket: process.env.BUCKET_NAME,
